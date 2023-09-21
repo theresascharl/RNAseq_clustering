002e4cd8f261bb05736318c7af7d067b16d5c48b
theresascharl <115463094+theresascharl@users.noreply.github.com>
2023-09-21 09:58:50 +0200

    Update fission_cluster_postprocessing.R
    new function names

--- fission_cluster_postprocessing.R
+++ fission_cluster_postprocessing.R
@@ -9,27 +9,26 @@ require("ggpubr")
 source("functions_cluster_postprocessing.R")
 
 ## select as many colors as clusters ------------------
-col <- hcl.colors(11)
-col1 <- alpha(col, alpha = 0.3)
+col <- hcl.colors(12)
 palette(col)
 
-## load cluster result ----
-load("fission_ALR_G-VVI-VV_11.RData")
+## load threeway cluster result ----
+load("fission_ALR_G-VVI-VV_12.RData")
 load("de_genes_fission_complete.RData")
 load("mprofiles_fission.RData")
 
 ## calculate dbsi ----
-res <- get.si(M, genes = genes)
+dbsi_3way <- get_dbsi(M, genes = genes)
 
-## silhouette plot ----
+## dbsi plot ----
 pdf("Figure3.pdf", width = 6, height = 6)
-silplot_pos(res$result)
+dbsi_plot(dbsi_3way$result)
 dev.off()
 
 ## cluster map ----
 pdf("Figure4.pdf", width = 6, height = 6)
 cluster_map(M = M, genes = genes, type = "3way", 
-            mprofiles = mprofiles, npcol = 4) 
+            mprofiles = mprofiles, npcol = 4)$plot 
 dev.off()
 
 
@@ -43,15 +42,15 @@ library("e1071")
 compareMatchedClasses(malr_VVV$classification, M$best.result[[1]]$id)$crand
 
 ## get dbsi for mclust ----
-res <- get.si(malr_VVV, genes = genes, type = "2way")
+resdbsi_2way <- get_dbsi(malr_VVV, genes = genes, type = "2way")
 
 pdf("Figure6a.pdf", width = 6, height = 6)
-silplot_pos(res$result)
+dbsi_plot(dbsi_2way$result)
 dev.off()
 
 ## cluster map ----
 pdf("Figure6b.pdf", width = 6, height = 6)
 cluster_map(M = malr_VVV, genes = genes, type = "2way", 
-            mprofiles = mprofiles, npcol = 2) 
+            mprofiles = mprofiles, npcol = 2)$plot 
 dev.off()
 
